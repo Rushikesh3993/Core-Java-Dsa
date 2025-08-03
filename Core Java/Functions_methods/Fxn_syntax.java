60288aef737f17d95bc82e79aa1148d0d5a3e802
Rushikesh Mote <moterushikesh39@gmail.com>
2025-08-03 18:12:09 +0530

    Function Syntax

---
+++ Core Java/Functions_methods/Fxn_syntax.java
@@ -0,0 +1,14 @@
+public class Fxn_syntax {
+
+
+    // here we write the function 
+    public static void  printHelloWorld() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+
+// here we call the function
+         printHelloWorld();
+    }
+}

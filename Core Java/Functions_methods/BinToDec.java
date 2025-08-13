c020939f3df5c3c2bb3d255a90a4e9b7e9a9d5ef
Rushikesh Mote <moterushikesh39@gmail.com>
2025-08-14 00:03:15 +0530

    binary to decimal

---
+++ Core Java/Functions_methods/BinToDec.java
@@ -0,0 +1,22 @@
+public class BinToDec {
+
+    public static void binToDec(int binNum){
+        int myNum = binNum;
+        int pow = 0;
+        int decNum = 0;
+
+        while (binNum > 0) {
+            int ldg = binNum % 10;
+            decNum = decNum + (ldg * (int)Math.pow(2, pow));
+
+            pow++;
+            binNum = binNum / 10;
+        }
+
+        System.out.println("decimal of " + myNum + " = " + decNum);
+    }
+    
+    public static void main(String [] args){
+        binToDec(1110);
+    }
+}

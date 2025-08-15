0e0b75943fec9e6cd6917acac10d6ba650f36a79
Rushikesh Mote <moterushikesh39@gmail.com>
2025-08-15 12:05:04 +0530

    even_odd check by functioin

---
+++ Function_question/Even_odd.java
@@ -0,0 +1,19 @@
+//  Write a method named isEven that accepts an int argument. The method should return true if the argument is even, or false otherwise. Also write a program to test your method
+
+package Function_question;
+
+public class Even_odd {
+    
+
+    public static boolean isEven(int n){
+        if(n % 2 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static void main(String []args){
+
+        System.out.println(isEven(4));
+    }
+}
